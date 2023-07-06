01c8eabdff0bbc5717d14bf5575708b4c6d7c87f
prashant <negiprashant606@gmail.com>
2023-07-06 12:44:36 +0530

    added promise on api

--- server.js
+++ server.js
@@ -14,51 +14,59 @@ app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 //POST
-app.post('/identify', async (req, res) => {
+app.post('/identify', (req, res) => {
   const { phoneNumber, email } = req.body;
-  client.query("SELECT * FROM contacts WHERE email='" + email + "' OR phonenumber='" + phoneNumber + "'", (error, results) => {
-    if (results.rows.length == 0) {
-      client.query('INSERT INTO contacts (phonenumber, email, "linkPrecedence") VALUES ($1, $2, $3)', [phoneNumber, email, "primary"], (error) => {
-        if (error) {
-          throw error
-        }
 
-      })
-    } else {
+  var getResult = new Promise((resolve, reject) => {
+    client.query("SELECT * FROM contacts WHERE email='" + email + "' OR phonenumber='" + phoneNumber + "'", (error, results) => {
+      if (results.rows.length == 0) {
+        client.query('INSERT INTO contacts (phonenumber, email, "linkPrecedence") VALUES ($1, $2, $3)', [phoneNumber, email, "primary"], (error) => {
+          if (error) {
+            reject(error)
+          }
+          resolve(results)
+        })
+      } else {
         client.query('INSERT INTO contacts ("linkedId", phonenumber, email, "linkPrecedence") VALUES ($1, $2, $3, $4)', [results.rows[0].id, phoneNumber, email, "secondary"], (error, results) => {
           if (error) {
-            throw error
+            reject(error)
           }
+          resolve(results)
         })
-    }
+      }
+    })
   })
 
-  client.query("SELECT * FROM contacts WHERE email='" + email + "' OR phonenumber='" + phoneNumber + "'", (error, results) => {
-    let email = []
-    let phoneNumbers = []
-    let secondaryContactIds = []
-    
-    results.rows.forEach((element) => {
-      if (!email.includes(element.email)) {
-        email.push(element.email)
-      }
-      if (!phoneNumbers.includes(element.phonenumber)) {
-        phoneNumbers.push(element.phonenumber)
-      }
-      if (!secondaryContactIds.includes(element.linkedId)) {
-        secondaryContactIds.push(element.linkedId)
-      }
-    });
+  getResult.then((data) => {
+    client.query("SELECT * FROM contacts WHERE email='" + email + "' OR phonenumber='" + phoneNumber + "'", (error, results) => {
+      let email = []
+      let phoneNumbers = []
+      let secondaryContactIds = []
 
-    let json = {
-      "contact": {
-        "primaryContatctId": results.rows[0].id,
-        "emails": email, // first element being email of primary contact 
-        "phoneNumbers": phoneNumbers, // first element being phoneNumber of primary contact
-        "secondaryContactIds": secondaryContactIds // Array of all Contact IDs that are "secondary" to the primary contact
+      results.rows.forEach((element) => {
+        if (!email.includes(element.email)) {
+          email.push(element.email)
+        }
+        if (!phoneNumbers.includes(element.phonenumber)) {
+          phoneNumbers.push(element.phonenumber)
+        }
+        if (!secondaryContactIds.includes(element.linkedId) && element.linkedId != null) {
+          secondaryContactIds.push(element.linkedId)
+        }
+      });
+
+      let json = {
+        "contact": {
+          "primaryContatctId": results.rows[0].id,
+          "emails": email, // first element being email of primary contact 
+          "phoneNumbers": phoneNumbers, // first element being phoneNumber of primary contact
+          "secondaryContactIds": secondaryContactIds // Array of all Contact IDs that are "secondary" to the primary contact
+        }
       }
-    }
-    return res.send(json);
+      return res.send(json);
+    })
+  }).catch((error) => {
+    return res.send(error);
   })
 });
 
